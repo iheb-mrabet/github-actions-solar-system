8e8a24f45fa548803915a2faa4f83cabcdfeb00d
Iheb Mrabet <ihabmrabet12@gmail.com>
2026-06-26 13:41:09 +0100

    archive always

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(cors())
 
 mongoose.connect(process.env.MONGO_URI, {
     user: process.env.MONGO_USERNAME,
-    pass: process.env.MONGO_PASSWORD,
+    pass: process.env.MONGO_PASSWORDd,
     useNewUrlParser: true,
     useUnifiedTopology: true
 }, function(err) {
